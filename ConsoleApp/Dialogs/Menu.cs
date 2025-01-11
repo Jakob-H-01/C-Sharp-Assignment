24f43ca3661868763097ea9ff259c14a777def32
Jakob Hjalmarsson <jakob.hjalmarsson@utb.ecutbildning.se>
2025-01-11 23:23:35 +0100

    Implemented dependency injection and started on the menu dialog

---
+++ ConsoleApp/Dialogs/Menu.cs
@@ -0,0 +1,47 @@
+﻿using Business.Interfaces;
+
+namespace ConsoleApp.Dialogs;
+
+public class Menu(IContactService contactService)
+{
+    private readonly IContactService _contactService = contactService;
+
+    public void Main()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("*** MENU ***");
+            Console.WriteLine("1. Add Contact");
+            Console.WriteLine("2. View Contacts");
+            Console.WriteLine("3. Quit");
+            Console.Write("\nSelect Action: ");
+
+            var action = Console.ReadLine();
+
+            switch (action)
+            {
+                case "1":
+                    AddContact();
+                    break;
+                
+                case "2":
+                    ViewContacts();
+                    break;
+                
+                case "3":
+                    break;
+            }
+        }
+    }
+
+    public void AddContact()
+    {
+        Console.Clear();
+    }
+
+    public void ViewContacts()
+    {
+        Console.Clear();
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Business.Interfaces;
+using Business.Services;
+using ConsoleApp.Dialogs;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<IFileService>(new FileService("contacts.json"))
+    .AddSingleton<IContactService, ContactService>()
+    .AddSingleton<Menu>()
+    .BuildServiceProvider();
+
+var menu = serviceProvider.GetRequiredService<Menu>();
+
+menu.Main();
