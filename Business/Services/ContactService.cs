1e0cfc603ab95ee300d3ad0d4ee43e33cf6b6941
Jakob Hjalmarsson <jakob.hjalmarsson@utb.ecutbildning.se>
2025-01-11 22:10:01 +0100

    Implemented services based on the created interfaces

---
+++ Business/Services/ContactService.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using Business.Interfaces;
+using Business.Models;
+
+namespace Business.Services;
+
+public class ContactService(IFileService fileService) : IContactService
+{
+    private readonly IFileService _fileService = fileService;
+    private List<Contact> _contacts = [];
+
+    public bool Add(Contact contact)
+    {
+        try
+        {
+            _contacts.Add(contact);
+            var json = JsonSerializer.Serialize(_contacts);
+            _fileService.Save(json);
+
+            return true;
+        }
+
+        catch { return false; }
+    }
+
+    public IEnumerable<Contact> GetAll()
+    {
+        try
+        {
+            var json = _fileService.Get();
+            _contacts = JsonSerializer.Deserialize<List<Contact>>(json)!;
+        }
+
+        catch { _contacts = []; }
+
+        return _contacts;
+    }
+}
---
+++ Business/Services/FileService.cs
@@ -0,0 +1,40 @@
+﻿using Business.Interfaces;
+
+namespace Business.Services;
+
+public class FileService : IFileService
+{
+    private readonly string _directoryPath;
+    private readonly string _filePath;
+
+    public FileService(string fileName)
+    {
+        _directoryPath = "Data";
+        _filePath = Path.Combine(_directoryPath, fileName);
+    }
+    public bool Save(string content)
+    {
+        try
+        {
+            if (!Directory.Exists(_directoryPath))
+            {
+                Directory.CreateDirectory(_directoryPath);
+            }
+
+            File.WriteAllText(_filePath, content);
+            return true;
+        }
+
+        catch { return false; }
+    }
+
+    public string Get()
+    {
+        if (File.Exists(_filePath))
+        {
+            return File.ReadAllText(_filePath);
+        }
+
+        return null!;
+    }
+}
