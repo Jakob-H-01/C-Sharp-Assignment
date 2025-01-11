c930d4b29d9c4c12a889624a0653bf3b59646144
Jakob Hjalmarsson <jakob.hjalmarsson@utb.ecutbildning.se>
2025-01-11 19:17:13 +0100

    Implemented a contact model and a Guid generator

---
+++ Business/Helpers/IdGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace Business.Helpers;
+
+public static class IdGenerator
+{
+    public static string Generate()
+    {
+        return Guid.NewGuid().ToString();
+    }
+}
---
+++ Business/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using Business.Helpers;
+
+namespace Business.Models;
+
+public class Contact
+{
+    public string? Id { get; set; } = IdGenerator.Generate();
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? StreetAddress { get; set; }
+    public string? ZipCode { get; set; }
+    public string? City { get; set; }
+}
