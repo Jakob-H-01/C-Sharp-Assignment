f07f5cd8560b4dfbfeeebd02e0a785847e0c4bc1
Jakob Hjalmarsson <jakob.hjalmarsson@utb.ecutbildning.se>
2025-01-12 21:41:53 +0100

    Wrote tests for guid generator and contact factory

---
+++ Business.Tests/Factories/ContactFactory_Tests.cs
@@ -0,0 +1,15 @@
+﻿using Business.Factories;
+using Business.Models;
+
+namespace Business.Tests.Factories;
+
+public class ContactFactory_Tests
+{
+    [Fact]
+    public void Create_ShouldReturnAContact()
+    {
+        var result = ContactFactory.Create();
+        Assert.NotNull(result);
+        Assert.IsType<Contact>(result);
+    }
+}
---
+++ Business.Tests/Helpers/IdGenerator_Tests.cs
@@ -0,0 +1,14 @@
+﻿using Business.Helpers;
+
+namespace Business.Tests.Helpers;
+
+public class IdGenerator_Tests
+{
+    [Fact]
+    public void Generate_ShouldReturnAStringContainingAGuid()
+    {
+        var result = IdGenerator.Generate();
+        Assert.NotNull(result);
+        Assert.True(Guid.TryParse(result, out _));
+    }
+}
