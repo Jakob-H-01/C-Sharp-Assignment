a5e278ae05ff8c643149129ba2fa4a3d0ad5db90
Jakob Hjalmarsson <jakob.hjalmarsson@utb.ecutbildning.se>
2025-01-12 19:55:37 +0100

    Implemented the two menu options

--- ConsoleApp/Dialogs/Menu.cs
+++ ConsoleApp/Dialogs/Menu.cs
@@ -1,4 +1,5 @@
-﻿using Business.Interfaces;
+﻿using Business.Factories;
+using Business.Interfaces;
 
 namespace ConsoleApp.Dialogs;
 
@@ -37,11 +38,60 @@ public class Menu(IContactService contactService)
 
     public void AddContact()
     {
+        var contact = ContactFactory.Create();
+
         Console.Clear();
+        Console.WriteLine("*** ADD CONTACT ***");
+
+        Console.Write("First Name: ");
+        contact.FirstName = Console.ReadLine();
+
+        Console.Write("Last Name: ");
+        contact.LastName = Console.ReadLine();
+
+        Console.Write("Email: ");
+        contact.Email = Console.ReadLine();
+
+        Console.Write("Phone Number: ");
+        contact.PhoneNumber = Console.ReadLine();
+
+        Console.Write("Street Address: ");
+        contact.StreetAddress = Console.ReadLine();
+
+        Console.Write("Zip Code: ");
+        contact.ZipCode = Console.ReadLine();
+
+        Console.Write("City: ");
+        contact.City = Console.ReadLine();
+
+        var result = _contactService.Add(contact);
+
+        if (!result)
+        {
+            Console.WriteLine("Something went wrong, unable to add contact");
+        }
+        else
+        {
+            Console.WriteLine("Contact successfully added!");
+        }
+
+        Console.ReadKey();
     }
 
     public void ViewContacts()
     {
+        var contacts = _contactService.GetAll();
+
         Console.Clear();
+        Console.WriteLine("*** CONTACTS ***");
+
+        var index = 0;
+        foreach (var contact in contacts)
+        {
+            Console.WriteLine($"{index + 1}. {contact.FirstName} {contact.LastName} {contact.Email} {contact.PhoneNumber} {contact.StreetAddress} {contact.ZipCode} {contact.City}");
+            index++;
+        }
+
+        Console.ReadKey();
     }
 }
